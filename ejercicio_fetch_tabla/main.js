0ff31219fe9d8cac36a6ce6a5cf11a8df19cb0f5
LeonelSandoval150 <leonelmaximilianosandoval@gmail.com>
2022-05-20 17:13:28 -0300

    tablas de fetch y ejercicio

---
+++ ejercicio_fetch_tabla/main.js
@@ -0,0 +1,27 @@
+//https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=
+
+const obtenr_usuarios = async () => {
+    const usuarios = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    const usuarios_definidos = await usuarios.json()
+   
+    let filas = []
+   
+   
+   usuarios_definidos.forEach(element => {
+   
+       let fila=
+       `<tr>
+       <td>${element.image}</td>
+       <td>${element.name}</td>
+       <td>${element.symbol}</td>
+       <td>${element.current_price}</td>
+       <td>${element.total_volume}</td>
+       </tr>`
+   
+       filas.push(fila)
+       
+   });
+   document.getElementById("tbl_body").innerHTML = filas.join('')
+   }
+   
+   obtenr_usuarios()
---
+++ fetch_tabla/main.js
@@ -0,0 +1,25 @@
+const obtenr_usuarios = async () => {
+ const usuarios = await fetch('https://jsonplaceholder.typicode.com/users')
+ const usuarios_definidos = await usuarios.json()
+
+ let filas = []
+
+// recorrer los usuarios
+// el array es una estructura repetitiva
+usuarios_definidos.forEach(element => {
+
+    let fila=
+    `<tr>
+    <td>${element.name}</td>
+    <td>${element.username}</td>
+    <td>${element.email}</td>
+    <td>${element.phone}</td>
+    </tr>`
+
+    filas.push(fila)
+    
+});
+document.getElementById("tbl_body").innerHTML = filas.join('')
+}
+// invocamos a la funcion
+obtenr_usuarios()
