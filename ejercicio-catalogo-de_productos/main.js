f1311cdef1142422f0d3c73676809debd172b239
LeonelSandoval150 <leonelmaximilianosandoval@gmail.com>
2022-05-27 17:07:56 -0300

    ejercicio de la tienda

---
+++ ejercicio-catalogo-de_productos/main.js
@@ -0,0 +1,29 @@
+const obtener_productos = async (dato) =>{
+
+  const datardos = await fetch("https://fakestoreapi.com/products/category/"+dato)
+  const elementos = await datardos.json()
+
+  let columnas = []
+
+  elementos.forEach(element => {
+      let columna = 
+      `
+      <div class="col-lg-3">
+          <div class="card">
+              <img height="300" src="${element.image}" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${element.title}</h5>
+                  <p class="card-text">$${element.price}</p>
+                  <p class="card-text">$${element.description}</p>
+                  <a href="#" class="btn btn-primary">Comprar</a>
+              </div>
+          </div>
+      </div>`
+
+      columnas.push(columna)
+
+  });
+
+  document.getElementById("catalogo").innerHTML = columnas.join('')
+
+}
