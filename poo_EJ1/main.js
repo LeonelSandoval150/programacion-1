8bf853ef17ed92964739b9d41675fa323051ac69
LeonelSandoval150 <leonelmaximilianosandoval@gmail.com>
2022-06-02 17:10:01 -0300

    mejora

--- poo_EJ1/main.js
+++ poo_EJ1/main.js
@@ -1,26 +1,28 @@
 // para el que el archivo mail se puedan importan cosas tiene que ser catalogado mululo
 //el mudulo esta conformado por muchas clases
-import Trapecio from "./trapecio.js";
 
 
+// nos importamos la clase Trapecio que proviene del archivo trapecio.js 
+ import Trapecio from "./trapecio.js";
 
-function invocar (dato1,dato1,dato3){
+     function ejecutar (){
 
+        const dato1=document.getElementById("inp_dato1").value
 
+        const dato2=document.getElementById("inp_dato2").value
 
-    //forma1
-    const dato1= document.querySelector("#inp_dato1").value
-    // forma2
-   const dato2= document.getElementById("inp_dato2").value
+        const dato3=document.getElementById("inp_dato3").value
 
-   const dato3= document.getElementById("inp_dato3").value
+            
+        // la instancia de la clase se considera un objeto y nos va apermitir el acceso ala metodo de la clase
 
-    // la instancia de la clase se considera un objeto y nos va apermitir el acceso ala metodo de la clase
+        // creamos la instancia de la clase  Trapecio
+            const trapecio = new Trapecio (dato1,dato2,dato3)
+            trapecio.calcular_area_trap()
+            // mediante el uso de la instancia invocamos y/o ejecutamos el metodo 
+            const respuesta = trapecio.calcular_area_trap()
 
-    const trapecio = new Trapecio (dato1,dato2,dato3)
-   const respuesta =  trapecio.calcular_trapecio()
+        document.getElementById("h_resultado").textContent='El trapecio es :'+respuesta
+     }
 
-   document.getElementById("h_resultado").textContent='El trapecio es :'+respuesta
-}
-
-document .getElementById("btn").addEventListener("click",invocar)
+ document .getElementById("btn").addEventListener("click",ejecutar)
--- poo_EJ1/trapecio.js
+++ poo_EJ1/trapecio.js
@@ -1,9 +1,12 @@
+//los atibutos hablan de las carateristicas
+
+
 //poner mayuscula si o si
 // export significa que lo voy a poder importar me lo tengo que acordar
-export default class Trapecio {
+ export default class Trapecio {
     // aca irian los atributos
 
-    // el  constructor  es un metodo y el el primero que ejecuta primero 
+    // el  constructor es un metodo y el el primero que ejecuta primero 
   constructor (dato1,dato2,dato3){
 
 // hay que almacenar lo que se trae (no me vas atraer algo y voy andar con eso todo el dia )
@@ -11,14 +14,13 @@ export default class Trapecio {
       this.b1= dato1
       this.b2= dato2
       this.b3= dato3
-
-
   }
 
-  calcular_trapecio (){
+   
+  calcular_area_trap (){
   
-    const suma= b1 + b2;
-    const mul= suma*b3;
+    const suma=parseInt(this.b1) +parseInt(this.b2) 
+    const mul= (suma*this.b3);
     const resultado = mul/2;
 
     return resultado
